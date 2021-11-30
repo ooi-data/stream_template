0e99ac008504096c44acc3531e1fd0ae14d30826
lsetiawan <landungs@uw.edu>
2021-11-30 13:01:10 -0800

    Add repo argument for a single update

--- .ci-helpers/config-updates.py
+++ .ci-helpers/config-updates.py
@@ -33,6 +33,64 @@ def _update_config(current_config_json, value={}):
     return updated_config, changes
 
 
+def config_update(repo, values, debug=True):
+    try:
+        print(repo.name)
+        config = repo.get_contents(
+            CONFIG_PATH_STR, ref=harvest_settings.github.main_branch
+        )
+        config_json = yaml.safe_load(config.decoded_content)
+        updated_config, changes = _update_config(config_json, value=values)
+        if debug:
+            print(f"Debug mode, updated config: {updated_config}")
+            if not changes:
+                print("No changes found... skipping update.")
+        else:
+            if not changes:
+                print("No changes found... skipping update.")
+            else:
+                process_status = repo.get_contents(
+                    PROCESS_STATUS_PATH_STR,
+                    ref=harvest_settings.github.main_branch,
+                )
+                request_status = repo.get_contents(
+                    REQUEST_STATUS_PATH_STR,
+                    ref=harvest_settings.github.main_branch,
+                )
+                request_status_json = yaml.safe_load(
+                    request_status.decoded_content
+                )
+                process_status_json = yaml.safe_load(
+                    process_status.decoded_content
+                )
+                print(
+                    "Request:",
+                    request_status_json['last_request'],
+                    request_status_json['status'],
+                )
+                print(
+                    "Process:",
+                    process_status_json['last_updated'],
+                    process_status_json['status'],
+                )
+                print("Updating config values...")
+                config_yaml = yaml.safe_dump(updated_config)
+                repo.update_file(
+                    CONFIG_PATH_STR,
+                    message="Update config values",
+                    content=config_yaml,
+                    sha=config.sha,
+                    branch=harvest_settings.github.main_branch,
+                )
+                print("Done.")
+        print()
+    except Exception as e:
+        print(repo.name)
+        print(f'File not found: {e}')
+        print(f"https://github.com/ooi-data/{repo.name}")
+        print()
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Perform Config Updates')
     parser.add_argument(
@@ -67,6 +125,11 @@ def parse_args():
         default=True,
         help='Not commit the changes',
     )
+    parser.add_argument(
+        '--repo',
+        type=str,
+        help='Specific repo to update config on',
+    )
 
     return parser.parse_args()
 
@@ -83,66 +146,16 @@ def main():
     gh = Github(harvest_settings.github.pat)
     print_rate_limiting_info(gh, 'GH_PAT')
     data_org = gh.get_organization(harvest_settings.github.data_org)
-    for repo in data_org.get_repos():
-        if repo.name != 'stream_template':
-            try:
-                config = repo.get_contents(
-                    CONFIG_PATH_STR, ref=harvest_settings.github.main_branch
-                )
-                process_status = repo.get_contents(
-                    PROCESS_STATUS_PATH_STR,
-                    ref=harvest_settings.github.main_branch,
-                )
-                request_status = repo.get_contents(
-                    REQUEST_STATUS_PATH_STR,
-                    ref=harvest_settings.github.main_branch,
-                )
-                config_json = yaml.safe_load(config.decoded_content)
-                updated_config, changes = _update_config(
-                    config_json, value=values
-                )
-                request_status_json = yaml.safe_load(
-                    request_status.decoded_content
-                )
-                process_status_json = yaml.safe_load(
-                    process_status.decoded_content
-                )
-                print(repo.name)
-                print(
-                    "Request:",
-                    request_status_json['last_request'],
-                    request_status_json['status'],
-                )
-                print(
-                    "Process:",
-                    process_status_json['last_updated'],
-                    process_status_json['status'],
-                )
-                if args.debug:
-                    print(f"Debug mode, updated config: {updated_config}")
-                    if not changes:
-                        print("No changes found... skipping update.")
-                else:
-                    if not changes:
-                        print("No changes found... skipping update.")
-                    else:
-                        print("Updating config values...")
-                        config_yaml = yaml.safe_dump(updated_config)
-                        repo.update_file(
-                            CONFIG_PATH_STR,
-                            message="Update config values",
-                            content=config_yaml,
-                            sha=config.sha,
-                            branch=harvest_settings.github.main_branch,
-                        )
-                        print("Done.")
-                print()
-                break
-            except Exception as e:
-                print(repo.name)
-                print(f'File not found: {e}')
-                print(f"https://github.com/ooi-data/{repo.name}")
-                print()
+    if args.repo:
+        try:
+            repo = data_org.get_repo(args.repo)
+            config_update(repo, values, args.debug)
+        except Exception:
+            raise ValueError(f"{args.repo} repository does not exist.")
+    else:
+        for repo in data_org.get_repos():
+            if repo.name != 'stream_template':
+                config_update(repo, values, args.debug)
 
 
 if __name__ == "__main__":
